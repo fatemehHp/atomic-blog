a406f02708d582d117fc9c3a881efc81477d8102
Fatemeh Hasanpour <hpfatemeh35@gmail.com>
2025-04-09 14:07:43 +0330

    create new atomic blog with new structure

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./style.css";
-import App from "./App";
+import AppLayout from "./component/AppLayout/AppLayout";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <AppLayout />
 );
